287db5217204172f01cec23503e200fb0ac405af
Christina Lavingia <christinalavingia@gmail.com>
2019-02-11 15:16:36 -0800

    Add dummy data for testing.

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var userInput = [
+    {
+        "name": "Jim",
+        "photo": "https://pbs.twimg.com/profile_images/3171824697/ef75d90df2e65ce326acf30262df5918_400x400.jpeg",
+        "scores": [
+            3,
+            1,
+            4,
+            1,
+            1,
+            1,
+            4,
+            2,
+            4,
+            4
+        ]
+    },
+    {
+        "name": "Pam",
+        "photo": "https://i.imgur.com/divKdXr.jpg",
+        "scores": [
+            1,
+            1,
+            4,
+            4,
+            1,
+            5,
+            3,
+            3,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Dwight",
+        "photo": "https://pbs.twimg.com/profile_images/1863312401/Dwight_400x400.jpg",
+        "scores": [
+            1,
+            5,
+            1,
+            5,
+            1,
+            1,
+            5,
+            1,
+            5,
+            1
+        ]
+    }
+];
+
+module.exports = userInput;
