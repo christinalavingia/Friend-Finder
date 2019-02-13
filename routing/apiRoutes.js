993e4549269995582b1191ae8fb8d90a77ad563a
Christina Lavingia <christinalavingia@gmail.com>
2019-02-13 11:32:40 -0800

    Update math logic.

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,31 +1,35 @@
-var path = require("path");
 var friends = require("../app/data/friends.js");
 
-module.exports = function(app) {
-    app.get("/api/friends", function(req, res) {
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
 
-    app.post("/api/friends", function(req,res) {
-        var userInput = req.body;
-        var userResponses = userInput.scores;
-        var matchName="";
-        var matchImage="";
-        var totalDifference = 10000; 
+    app.post("/api/friends", function (req, res) {
+        var userInput = req.body.scores;
+        var scoresArray = [];
+        var friendCount = 0;
+        var bestMatch = 0;
+        
 
         for (var i = 0; i < friends.length; i++) {
-            var diff = 0;
-            for (var j = 0; j < userResponses.length; j++) {
-                diff += Math.abs(friends[i].scores[i] - userResponses[j]);
+            var difference = 0;
+            for (var j = 0; j < userInput.length; j++) {
+                difference += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(userInput[j])));
             }
-            if (diff < totalDifference) {
-                totalDifference = diff;
-                matchName = friends[i].name;
-                matchImage = friends[i].photo;
-            }
-        }
-        friends.push(userInput);
 
-        res.json({status: "OK", matchName: matchName, matchImage: matchImage});
+            scoresArray.push(difference);
+
+        }
+
+            for (var i = 0; i < scoresArray.length; i++) {
+                if (scoresArray[i] <= scoresArray[bestMatch]) {
+                    bestMatch = i;
+                }
+            }
+     
+            var bestFriend = friends[bestMatch];
+            res.json(bestFriend);
+            friends.push(req.body);
     });
 };
