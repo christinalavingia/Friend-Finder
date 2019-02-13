e0f8aafbcab981ecb7e747adae66c004b9a167d4
Christina Lavingia <christinalavingia@gmail.com>
2019-02-13 10:33:12 -0800

    Format document and remove commented out code.

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -2,9 +2,8 @@ var path = require("path");
 var express = require("express");
 
 module.exports = function (app) {
-    // app.use(express.static(__dirname + '/public'));
 
-    app.get("/survey", function (req,res) {
+    app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"))
     });
 
